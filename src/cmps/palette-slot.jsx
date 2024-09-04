799fc1664998f626c6d4a1de07a3f4b74a70b962
Karin Zohar <karinzohar3@gmail.com>
2024-09-04 14:53:43 +0300

    feat: improve palette design and add hover effect

--- src/cmps/palette-slot.jsx
+++ src/cmps/palette-slot.jsx
@@ -1,8 +1,8 @@
 export function PaletteSlot({slot}) {
 
     return(
-        <article className="palette-slot h-full flex justify-center items-center" style={{backgroundColor: slot.hexCode}}>
-            <p className="h-max">{slot.colorName}</p>
+        <article className="palette-slot h-full flex justify-center items-center p-1 text-white" style={{backgroundColor: slot.hexCode}}>
+            <p className="h-max text-center p-1 rounded hover:bg-gray-300 hover:bg-opacity-50 cursor-pointer" >{slot.colorName}</p>
         </article>
     )
 }
